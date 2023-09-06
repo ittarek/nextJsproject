ad8c648c9493ff0c17fc2ff45ef66ffebb2089db
MD Tariqul Islam <ittarek551@gmail.com>
2023-09-06 17:45:23 +0600

    json data link add

--- src/utils/getAllNews.js
+++ src/utils/getAllNews.js
@@ -1,6 +1,6 @@
 const getAllNews = async (category_id) => {
   // let url = "http://localhost:5000/db.json";
-  let url = "https://next-jsproject-theta.vercel.app/db.json";
+  let url = "https://vercel.com/ittarek/next-jsproject/CLabEMDvkzrBz8tn9LGJ3QRrxf22/source?f=src%2Fpublic%2Fdb.json";
 
   if (category_id) {
     url += "?category_id=" + category_id;
