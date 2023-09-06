24cc6f33bedbd9895c478727e81258385130f42c
MD Tariqul Islam <ittarek551@gmail.com>
2023-09-06 18:26:35 +0600

    meta data

--- src/app/news/[id]/page.js
+++ src/app/news/[id]/page.js
@@ -2,25 +2,26 @@ import SingleNews from '@/components/HomeSection/SingleNews';
 import getAllNews from '@/utils/getAllNews';
 import React from 'react';
 
-const newsPage =  ({searchParams}) => {
+const newsPage = async ({searchParams}) => {
     // const allNews = await getAllNews(searchParams.category_id)
-    const allNews = "db.json"
+    const allNews = 
     
-    // [
-    //     {
-    //         "name": "tarek",
-    //         "id" : 1
-    //     },
-    //     {
-    //         "name": "tarek",
-    //         "id" : 2
-    //     },
-    //     {
-    //         "name": "tarek",
-    //         "id" : 3
-    //     }
     
-    // ]
+    [
+        {
+            "name": "tarek",
+            "id" : 1
+        },
+        {
+            "name": "tarek",
+            "id" : 2
+        },
+        {
+            "name": "tarek",
+            "id" : 3
+        }
+    
+    ]
     return (
         <div className='container mx-auto'>
             {
